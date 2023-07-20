434f31a947d7c3a3d22a72d1d6b2a13235fd2d02
MateuszMyszke <myszke93@gmail.com>
2023-07-20 15:40:24 +0200

    Add serach function

--- client/src/components/features/SearchForm/SearchForm.js
+++ client/src/components/features/SearchForm/SearchForm.js
@@ -1,14 +1,24 @@
-import Button from '../Button/Button';
-import SearchInput from '../SearchInput/SearchInput';
-import styles from './SearchForm.module.scss';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { searchAd } from '../../../redux/adsRedux';
+import { Form, Button } from 'react-bootstrap';
 
-const SearchForm = () => {
-  return(
-    <form className={styles.search}>
-      <SearchInput placeholder='enter a phrasesearch' />
-      <Button>Search</Button>
-    </form>
-  )
+const SearchForm= () => {
+  const [searchPhrase, setSearchPhrase] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSearch = () => {
+    dispatch(searchAd(searchPhrase));
+  };
+
+  return (
+    <div className="d-flex">
+      <Form.Group controlId="searchForm">
+        <Form.Control type="text" value={searchPhrase} onChange={(e) => setSearchPhrase(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" onClick={handleSearch} className="mx-2">Search</Button>
+    </div>
+  );
 };
 
 export default SearchForm;
--- client/src/components/features/SearchInput/SearchInput.js
+++
@@ -1,7 +0,0 @@
-import styles from './SearchInput.module.scss';
-
-const SearchInput = props => {
-  return (<input className={styles.input} placeholder={props.placeholder} value={props.value} onChange={props.onChange} type="text" />)
-}
-
-export default SearchInput;
